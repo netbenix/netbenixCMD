07094cea1d1e0cc2f7c6d26d85eec4a06a77a7ff
netbenixcn <netbenix@codenoodles.tk>
2020-06-07 11:15:56 +0200

    Changed Logger

--- utils/logger.c
+++ utils/logger.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <linux/limits.h>
 #include "logger.h"
 
+char loggerDirPath[PATH_MAX];
 
 void logger(char message[512]){
 	FILE *log_file;
 	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
-	log_file = fopen("output.log", "a");
+	char buffer[1024];
+	snprintf(buffer, sizeof(buffer), "%s/output.log", loggerDirPath);
+	log_file = fopen(buffer, "a");
 	fprintf(log_file, "[%d-%02d-%02d %02d:%02d:%02d] ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 	fprintf(log_file, "%s\n", message);
 	fclose(log_file);
